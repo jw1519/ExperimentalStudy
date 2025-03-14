71657308c49dd21b9607037a8f6e19f256e0d139
Jess Woodward <JW1519@student.aru.ac.uk>
2025-03-14 15:22:21 +0000

    canvas change to screen space if no vr device

--- Assets/scripts/CheckDevice.cs
+++ Assets/scripts/CheckDevice.cs
@@ -1,32 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.XR.OpenVR;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Management;
 
 public class CheckDevice : MonoBehaviour
 {
     List<Canvas> canvasList = new List<Canvas>();
     private void Awake()
     {
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            foreach (Canvas canvas in canvasList)
-            {
-                canvas.renderMode = RenderMode.ScreenSpaceCamera;
-                canvas.worldCamera = Camera.main;
-            }
-        }
-        else if (Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-            foreach (Canvas canvas in canvasList)
-            {
-                canvas.renderMode = RenderMode.ScreenSpaceCamera;
-                canvas.worldCamera = Camera.main;
-            }
-        }
-        else
-        {
-            Debug.Log("Unknown");
-        }
+        CheckForDevice();
     }
     public void AddCanvas(Canvas canvas)
     {
@@ -34,6 +18,44 @@ public class CheckDevice : MonoBehaviour
     }
     public void CheckForDevice()
     {
-
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            foreach (Canvas canvas in canvasList)
+            {
+                canvas.renderMode = RenderMode.ScreenSpaceCamera;
+                Debug.Log("mobile device");
+            }
+        }
+        else if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            foreach (Canvas canvas in canvasList)
+            {
+                canvas.renderMode = RenderMode.ScreenSpaceCamera;
+                Debug.Log("mobile device");
+            }
+        }
+        //checks if vr headset is connected
+        else if (XRGeneralSettings.Instance.Manager.activeLoader != null)
+        {
+            foreach (Canvas canvas in canvasList)
+            {
+                canvas.renderMode = RenderMode.WorldSpace;
+                canvas.worldCamera = Camera.main;
+                Debug.Log("Not a mobile device");
+            }
+        }
+        else
+        {
+            foreach (Canvas canvas in canvasList)
+            {
+                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                int child = canvas.transform.childCount;
+                for (int i = 0; i < child; i++)
+                {
+                    canvas.transform.GetChild(i).localScale = canvas.transform.GetChild(i).localScale * 100;
+                }
+                Debug.Log("other");
+            }
+        }
     }
 }
